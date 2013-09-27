2b2660ae98b8900e4b7f7d5332d4dc7932eba130
kaotisk-hund <ccc2007chaos@gmail.com>
2013-09-27 13:04:31 +0300

    [update] - Prototype class. Not fully implemented.

---
+++ class/ticket.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ticket
+ *
+ * @author kaotisk-hund
+ */
+class ticket {
+    public $id, $title, $description, $project_assigned, $status;
+    
+    function showTicket(){
+        
+    }
+    
+    function updateTicket(){
+        
+    }
+    
+    function deleteTicket(){
+        
+    }
+}
+
+?>
